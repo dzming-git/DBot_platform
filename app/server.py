a6d2ff6aa507ea86d677e218a06c297dd5b588a8
dzming-git <dzm_work@163.com>
2023-03-18 00:24:11 +0800

    将consul的token配置在文件中

--- app/server.py
+++ app/server.py
@@ -8,6 +8,7 @@ from DBot_SDK import server_thread
 
 def load_conf():
     ConfigFromUser.RouteInfo_load_config('conf/route_info/route_info.yaml')
+    ConfigFromUser.ConsulInfo_load_config('conf/consul_info/consul_info.yaml')
 
 if __name__ == '__main__': 
     load_conf()

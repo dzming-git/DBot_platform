bda9a2f7d1a76e8f1946617ab58c204523e184d8
dzming-git <dzm_work@163.com>
2023-03-19 01:01:09 +0800

    修改SDK的调用方式

--- app/server.py
+++ app/server.py
@@ -3,15 +3,12 @@ import time
 
 from DBot_SDK import ConfigFromUser
 ConfigFromUser.is_message_broker(True)
+ConfigFromUser.RouteInfo_load_config('conf/route_info/route_info.yaml')
+ConfigFromUser.ConsulInfo_load_config('conf/consul_info/consul_info.yaml')
 
 from DBot_SDK import server_thread
 
-def load_conf():
-    ConfigFromUser.RouteInfo_load_config('conf/route_info/route_info.yaml')
-    ConfigFromUser.ConsulInfo_load_config('conf/consul_info/consul_info.yaml')
-
 if __name__ == '__main__': 
-    load_conf()
     server_thread.set_safe_start(True)
     if server_thread.start():
         while True:

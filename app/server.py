0cc0d595057eece9beda76e75175b080d2118a97
dzming-git <dzm_work@163.com>
2023-03-24 01:19:10 +0800

    兼容DBot2.0

--- app/server.py
+++ app/server.py
@@ -7,6 +7,4 @@ if __name__ == '__main__':
     dbot.is_platform(True)
     dbot.set_routeInfo_config('conf/route_info/route_info.yaml')
     dbot.set_consulInfo_config('conf/consul_info/consul_info.yaml')
-    if dbot.start_server():
-        while True:
-            time.sleep(10)
+    dbot.start()

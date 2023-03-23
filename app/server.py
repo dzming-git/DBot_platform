74630df8d18b0d7c978afb9aba1fbfe4ceec387d
dzming-git <dzm_work@163.com>
2023-03-23 21:08:43 +0800

    兼容SDK版本1.2至1.5之间的修改

--- app/server.py
+++ app/server.py
@@ -1,15 +1,12 @@
 # service.py
 import time
-
-from DBot_SDK import ConfigFromUser
-ConfigFromUser.is_message_broker(True)
-ConfigFromUser.RouteInfo_load_config('conf/route_info/route_info.yaml')
-ConfigFromUser.ConsulInfo_load_config('conf/consul_info/consul_info.yaml')
-
-from DBot_SDK import server_thread
+from dbot import DBot
 
 if __name__ == '__main__': 
-    server_thread.set_safe_start(True)
-    if server_thread.start():
+    dbot = DBot()
+    dbot.is_platform(True)
+    dbot.set_routeInfo_config('conf/route_info/route_info.yaml')
+    dbot.set_consulInfo_config('conf/consul_info/consul_info.yaml')
+    if dbot.start_server():
         while True:
             time.sleep(10)

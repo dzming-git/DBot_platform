4febb4e45000424be6846b94ef4bbcf56bc7b02f
dzming-git <dzm_work@163.com>
2023-03-29 11:19:11 +0800

    兼容dbot的修改

--- app/server.py
+++ app/server.py
@@ -5,6 +5,6 @@ from dbot import DBot
 if __name__ == '__main__': 
     dbot = DBot()
     dbot.is_platform(True)
-    dbot.set_routeInfo_config('conf/route_info/route_info.yaml')
-    dbot.set_consulInfo_config('conf/consul_info/consul_info.yaml')
+    dbot.set_route_info_config('conf/route_info/route_info.yaml')
+    dbot.set_consul_info_config('conf/consul_info/consul_info.yaml')
     dbot.start()

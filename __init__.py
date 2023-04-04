7e05e1fdead20e5831c0bdfe1ecd8e2ffc625ce3
dzming-git <dzm_work@163.com>
2023-04-04 23:12:19 +0800

    兼容DzMicro2.0.1的修改

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from my_platform.app import server
--- app/server.py
+++ app/server.py
@@ -1,10 +1,10 @@
 # service.py
-import time
 from dbot import DBot
 
 if __name__ == '__main__': 
     dbot = DBot()
     dbot.is_platform(True)
-    dbot.set_routeInfo_config('conf/route_info/route_info.yaml')
-    dbot.set_consulInfo_config('conf/consul_info/consul_info.yaml')
-    dbot.start()
+    dbot.set_route_info_config('conf/route_info/route_info.yaml')
+    dbot.set_consul_info_config('conf/consul_info/consul_info.yaml')
+    dbot.set_mq_info_config('conf/mq_info/mq_info.yaml')
+    dbot.start_bot()

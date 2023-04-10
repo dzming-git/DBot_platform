6d91180432e8a8face591eb437db4d2097a843b7
dzming-git <dzm_work@163.com>
2023-04-10 16:16:58 +0800

    兼容dzmicro2.0.2

--- app/server.py
+++ app/server.py
@@ -2,8 +2,7 @@
 from dbot import DBot
 
 if __name__ == '__main__': 
-    dbot = DBot()
-    dbot.is_platform(True)
+    dbot = DBot(True)
     dbot.set_route_info_config('conf/route_info/route_info.yaml')
     dbot.set_consul_info_config('conf/consul_info/consul_info.yaml')
     dbot.set_mq_info_config('conf/mq_info/mq_info.yaml')
